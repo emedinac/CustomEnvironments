b1eec0c4526acd00eea1aa78c3a1fb9968b643c4
edgarmedina1801 <edgarmedina1801@gmail.com>
2019-11-06 09:09:34 +0100

    running

--- PPG.py
+++ PPG.py
@@ -9,7 +9,7 @@ counter = 0
 reward = None
 while reward != 50:
     env.render()
-    actions = np.random.choice([-1,1],13)
+    actions = np.random.choice([-1,1],env.action_space.shape)
     # state, reward, done, info = env.step(env.action_space.sample())
     state, reward, done, info = env.step(actions) 
     counter += 1
