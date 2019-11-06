7c59cc4763cad8ae2b37c895c7841788209829a2
edgarmedina1801 <edgarmedina1801@gmail.com>
2019-11-06 16:36:10 +0100

    running

--- TestEnv.py
+++ TestEnv.py
@@ -7,12 +7,14 @@ env = gym.make('valves-v0')
 state = env.reset()
 counter = 0
 reward = None
-while reward != 50:
+while counter != 50000:
     env.render()
     actions = np.random.choice([-1,1],env.action_space.shape)
     # state, reward, done, info = env.step(env.action_space.sample())
     state, reward, done, info = env.step(actions) 
     counter += 1
-    print(counter, actions)
-
-
+    if done:
+        state = env.reset()
+    if counter%5000==0:
+        state = env.reset()
+    print(counter, done, reward)
