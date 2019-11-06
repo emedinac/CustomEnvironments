9b8e5fa775c7324f2295dbc416a6c31ad29edb50
edgarmedina1801 <edgarmedina1801@gmail.com>
2019-11-06 09:03:28 +0100

    running

--- PPG.py
+++ PPG.py
@@ -1,4 +1,18 @@
 import gym
 import CustomEnv
+import numpy as np
 
 env = gym.make('valves-v0')
+
+state = env.reset()
+counter = 0
+reward = None
+while reward != 50:
+    env.render()
+    actions = np.random.choice([-1,1],13)
+    # state, reward, done, info = env.step(env.action_space.sample())
+    state, reward, done, info = env.step(actions) 
+    counter += 1
+    print(counter, actions)
+
+
